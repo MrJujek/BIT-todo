e27389b8bad1dee0c761c255393b32abe9123d7d
MrJujek <mr.jujek@gmail.com>
2024-10-16 00:15:30 +0200

    🔧 Add NextUI

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,8 +3,12 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import {NextUIProvider} from "@nextui-org/react";
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <NextUIProvider>
+      <App />
+    </NextUIProvider>
   </StrictMode>,
 )
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}"
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [
+    nextui()
+  ],
 }
 
