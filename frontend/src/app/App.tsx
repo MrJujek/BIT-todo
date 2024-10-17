18a2f5a73bef88992be7cdf830c15c2bafe9609f
MrJujek <mr.jujek@gmail.com>
2024-10-17 17:32:08 +0200

    ✨ Add protected route

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,13 +1,37 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate, Outlet } from 'react-router-dom';
 import TodoList from './pages/TodoList';
 import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import { useEffect, useState } from 'react';
+
+function ProtectedRoute() {
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState<string | null>(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setUser('user');
+      setLoading(false);
+    }, 0)
+  }, [])
+
+  return (
+    <>
+      {loading ? <div>Loading...</div> :
+        user ? <Outlet /> : <Navigate to="/login" replace />}
+    </>
+  )
+}
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<TodoList />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/" element={<TodoList />} />
+        </Route>
         <Route path="/login" element={<Login />} />
+        <Route path="/404" element={<NotFound />} />
         <Route path="*" element={<Navigate to="/404" replace />} />
       </Routes>
     </BrowserRouter>
