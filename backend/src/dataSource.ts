b76b86fe6fc74a1816a53080f829bbd119bd8448
Julian Dworzycki <mr.jujek@gmail.com>
2024-10-19 21:34:07 +0200

    ➕ Configure .env

--- backend/src/dataSource.ts
+++ backend/src/dataSource.ts
@@ -1,14 +1,21 @@
 import { DataSource } from 'typeorm';
 import { User } from './entity/User';
 import { Todo } from './entity/Todo';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+const database = process.env.DB_DATABASE;
 
 export const AppDataSource = new DataSource({
   type: 'mysql',
   host: 'localhost',
   port: 3306,
-  username: 'root',
-  password: 'zaq1@WSX',
-  database: 'todos',
+  username: username,
+  password: password,
+  database: database,
   synchronize: true,
   entities: [User, Todo],
 });
